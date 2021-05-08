777ad994db7b027e332a29eaa7e3614e244ff63d
Alexander Janßen <janssen@myswooop.de>
2021-05-08 19:31:30 +0200

    first draft on bird input

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Birds
 
+Project to handle all observed birds.
+
+
+
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.12.
 
 ## Development server
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BirdsComponent } from './birds/birds.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BirdsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
